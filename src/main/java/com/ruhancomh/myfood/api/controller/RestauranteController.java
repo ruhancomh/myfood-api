668b5bd8550428e5d28c20ae741942a5f801450e
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2020-12-01 19:38:30 -0300

    Criando cadastro de restaurantes

--- src/main/java/com/ruhancomh/myfood/api/controller/RestauranteController.java
+++ src/main/java/com/ruhancomh/myfood/api/controller/RestauranteController.java
@@ -3,9 +3,12 @@ package com.ruhancomh.myfood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -31,6 +34,18 @@ public class RestauranteController {
 			return ResponseEntity.ok(this.cadastroRestauranteService.buscar(id));
 		} catch (RecursoNaoEncontradoException e) {
 			return ResponseEntity.notFound().build();
+			
+		}
+	}
+	
+	@PostMapping
+	public ResponseEntity<?> criar (@RequestBody Restaurante restaurante) {
+		try {
+			Restaurante restauranteCriado = this.cadastroRestauranteService.criar(restaurante);
+			return ResponseEntity.status(HttpStatus.CREATED).body(restauranteCriado);
+			
+		} catch (RecursoNaoEncontradoException e) {
+			return ResponseEntity.badRequest().body(e.getMessage());
 			
 		}
 	}
--- src/main/java/com/ruhancomh/myfood/domain/service/CadastroRestauranteService.java
+++ src/main/java/com/ruhancomh/myfood/domain/service/CadastroRestauranteService.java
@@ -6,7 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ruhancomh.myfood.domain.exception.RecursoNaoEncontradoException;
+import com.ruhancomh.myfood.domain.model.Cozinha;
 import com.ruhancomh.myfood.domain.model.Restaurante;
+import com.ruhancomh.myfood.domain.repository.CozinhaRepository;
 import com.ruhancomh.myfood.domain.repository.RestauranteRepository;
 
 @Service
@@ -14,6 +16,9 @@ public class CadastroRestauranteService {
 
 	@Autowired
 	private RestauranteRepository restauranteRepository;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	public List<Restaurante> listar () {
 		return this.restauranteRepository.listar();
@@ -27,5 +32,18 @@ public class CadastroRestauranteService {
 		}
 		
 		return restaurante;
+	}
+	
+	public Restaurante criar (Restaurante restaurante)  {	
+		Long cozinhaId = restaurante.getCozinha().getId();
+		Cozinha cozinha = this.cozinhaRepository.buscar(cozinhaId);
+		
+		if (cozinha == null) {
+			throw new RecursoNaoEncontradoException("cozinha", cozinhaId);
+		}
+		
+		restaurante.setCozinha(cozinha);
+		
+		return this.restauranteRepository.salvar(restaurante);	
 	}
 }
