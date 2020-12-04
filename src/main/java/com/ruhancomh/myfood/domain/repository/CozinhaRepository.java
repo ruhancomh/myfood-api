e02d25664619b749ff14ba58528619a347df7305
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2020-12-04 17:44:21 -0300

    Adicionando busca customizada no repositorio de cozinhas

--- src/main/java/com/ruhancomh/myfood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/ruhancomh/myfood/domain/repository/CozinhaRepository.java
@@ -1,5 +1,7 @@
 package com.ruhancomh.myfood.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,4 +9,7 @@ import com.ruhancomh.myfood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long>{	
+
+	public List<Cozinha> findByNome(String nome);
+	
 }
