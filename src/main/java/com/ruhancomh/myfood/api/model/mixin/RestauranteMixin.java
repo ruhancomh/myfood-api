7661bfc960537d483fad3fb77e49c5a47425d386
Ruhan de Oliveira Baiense <rdgitarra@gmail.com>
2020-12-12 15:11:28 -0300

    fix

--- src/main/java/com/ruhancomh/myfood/api/model/mixin/RestauranteMixin.java
+++ src/main/java/com/ruhancomh/myfood/api/model/mixin/RestauranteMixin.java
@@ -5,7 +5,6 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.ruhancomh.myfood.domain.model.Cozinha;
-import com.ruhancomh.myfood.domain.model.Endereco;
 import com.ruhancomh.myfood.domain.model.FormaPagamento;
 import com.ruhancomh.myfood.domain.model.Produto;
 
